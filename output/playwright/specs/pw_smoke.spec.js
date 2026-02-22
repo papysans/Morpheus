251b250f62db19d9a278df8b6bcc4bf77c69c356
papysans <807399089@qq.com>
2026-02-22 16:28:32 +0800

    chore: 从版本控制中移除 output 文件夹

--- output/playwright/specs/pw_smoke.spec.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('open home', async ({ page }) => {
-  await page.goto('http://localhost:3002');
-  await expect(page.getByRole('heading', { name: '创作项目' })).toBeVisible();
-});
